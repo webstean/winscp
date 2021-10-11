c7e33716eb0311c87969c04a624793bdadd30be2
Andrew Webster <webstean@gmail.com>
2021-10-11 15:23:17 +1100

    reformat

--- watchthencopy.ps1
+++ watchthencopy.ps1
@@ -1,52 +1,55 @@
-# Set path to winscp 
-    $assemblyPath = "C:\Program Files (x86)\WinSCP"
-    Add-Type -Path (Join-Path $assemblyPath "WinSCPnet.dll")
+# Set path to winscp 0
+$assemblyPath = "C:\Program Files (x86)\WinSCP"
+Add-Type -Path (Join-Path $assemblyPath "WinSCPnet.dll")
 
-    # Setup session options
-    $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
-        Protocol = [WinSCP.Protocol]::Sftp
-        HostName = "hostname"
-        UserName = "username"
-        Password = "passwd"
-        SshHostKeyFingerprint = "ssh-rsa 2048 xx:xx"
-    }
+# Setup session options
+$sessionOptions = New-Object WinSCP.SessionOptions -Property @{
+  Protocol = [WinSCP.Protocol]::Scp
+  HostName = "hostname"
+  UserName = "username"
+  Password = "passwd"
+  SshHostKeyFingerprint = "ssh-rsa 2048 xx:xx"
+}
 
-    $session = New-Object WinSCP.Session
+# Destinatoon for file
+# $remotePath = "/remotefilegoeshere"
+$remotePath = "/"
 
-    try
-    {
-      # Connect
-      $session.Open($sessionOptions)
-      # Upload files, collect results
-      $transferOptions = New-Object WinSCP.TransferOptions
-      $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
+$session = New-Object WinSCP.Session
 
-      $transferResult =
-          $session.PutFiles($filepath, $remotePath, $False, $transferOptions)
+try {
+    # Connect
+    $session.Open($sessionOptions)
+    # Upload files, collect results
+    $transferOptions = New-Object WinSCP.TransferOptions
+    $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
 
-      # Throw on any error
-      $transferResult.Check()
+    $transferResult =
+    $session.PutFiles($filepath, $remotePath, $False, $transferOptions)
 
-      # Print results
-      foreach ($transfer in $transferResult.Transfers)
-      {
+    # Throw on any error
+    $transferResult.Check()
+
+    # Print results
+    foreach ($transfer in $transferResult.Transfers) {
           Write-Host "Upload of $($transfer.FileName) succeeded"
-      }
     }
-    finally
-    {
-      # Disconnect, clean up
-      $session.Dispose()
-    }
+}
+  
+finally {
+  # Disconnect, clean up
+  $session.Dispose()
+}
 
-    exit 0
+exit 0
   }#end of first try 
-  catch
-    {
+
+catch {
     Write-Host "Error: $($_.Exception.Message)"
     exit 1
-    }
-  }
+}
+
+}
 
 }#end of action
 
